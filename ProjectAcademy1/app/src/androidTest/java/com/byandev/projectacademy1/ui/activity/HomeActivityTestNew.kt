4ca39dd989497d3458d3804cb83ec18252fd7c24
byansanur <ratbyansa25@gmail.com>
2020-10-16 23:21:36 +0700

    fixing HomeActivity instrument test

--- ProjectAcademy1/app/src/androidTest/java/com/byandev/projectacademy1/ui/activity/HomeActivityTestNew.kt
+++ ProjectAcademy1/app/src/androidTest/java/com/byandev/projectacademy1/ui/activity/HomeActivityTestNew.kt
@@ -1,20 +1,19 @@
 package com.byandev.projectacademy1.ui.activity
 
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.rule.ActivityTestRule
 import com.byandev.projectacademy1.R
 import com.byandev.projectacademy1.utils.DataDummy
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
-@Suppress("DEPRECATION")
-class HomeActivityTest {
-
+class HomeActivityTestNew {
     /*
     Scenario Instrument Test / UI Test
     1. Menampilkan data kursus academy
@@ -57,7 +56,6 @@ class HomeActivityTest {
     @get:Rule
     var activityRule = ActivityTestRule(HomeActivity::class.java)
 
-
     /*
     Jika diperhartikan, di sini terdapat kode RecyclerViewActions yang digunakan untuk pengujian
     pada RecyclerView seperti scroll ke posisi tertentu dan klik pada posisi tertentu.
@@ -67,43 +65,64 @@ class HomeActivityTest {
     @Test
     fun loadCourse() {
         // test for load data with espresso
-        onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_academy))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_academy))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
 
     }
 
     @Test
     fun loadDetailCourse() {
         // perform click terhadap item recyclerView click()
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
+                ViewActions.click()
+            ))
         // check apakah text tampil dengan baik dengan isDisplay()
-        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.text_title))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.text_date))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         // check apakah text yang tampil sesuai dengan data yang ada
-        onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
-        onView(withId(R.id.text_date)).check(matches(withText("Deadline ${dummyCourse[0].deadline}")))
+        Espresso.onView(ViewMatchers.withId(R.id.text_title))
+            .check(ViewAssertions.matches(ViewMatchers.withText(dummyCourse[0].title)))
+        Espresso.onView(ViewMatchers.withId(R.id.text_date))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Deadline ${dummyCourse[0].deadline}")))
     }
 
     @Test
     fun loadModule() {
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.btn_start)).perform(click())
-        onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
+                ViewActions.click()
+            ))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_start)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.rv_module))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
     fun loadDetailModule() {
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.btn_start)).perform(click())
-        onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.web_view)).check(matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
+                ViewActions.click()
+            ))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_start)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.rv_module)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
+                ViewActions.click()
+            ))
+        Espresso.onView(ViewMatchers.withId(R.id.web_view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
     fun loadBookmarks() {
-        onView(withText("Bookmark")).perform(click())
-        onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        Espresso.onView(ViewMatchers.withText(R.string.bookmark)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.rv_bookmark))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.rv_bookmark))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
     }
-
 }
