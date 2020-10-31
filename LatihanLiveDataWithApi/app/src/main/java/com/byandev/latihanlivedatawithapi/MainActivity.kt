3f54854774a6350868de7c1b0f0195ff6803a9ff
byansanur <ratbyansa25@gmail.com>
2020-10-31 22:43:50 +0700

    live data and mutable live data with retrofit : - get data - post data

--- LatihanLiveDataWithApi/app/src/main/java/com/byandev/latihanlivedatawithapi/MainActivity.kt
+++ LatihanLiveDataWithApi/app/src/main/java/com/byandev/latihanlivedatawithapi/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.byandev.latihanlivedatawithapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel : MainViewModel by viewModels()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // prepare liveData with api
+
+        supportActionBar?.hide()
+
+        mainViewModel.findRestaurant()
+        mainViewModel.restaurant.observe(this, {
+            tvTitle.text = it.name
+            tvDescription.text = "${it.description.take(100)}..."
+            Glide.with(this)
+                .load("https://restaurant-api.dicoding.dev/images/large/${it.pictureId}")
+                .into(ivPicture)
+        })
+
+        mainViewModel.listReview.observe(this, { it ->
+            val listReview = ArrayList<String>()
+            if (!it.isNullOrEmpty()) {
+                it.forEach {
+                    listReview.add("${it.review}\n- ${it.name}")
+                }
+                lvReview.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listReview)
+            }
+        })
+        mainViewModel.isLoading.observe(this, {
+            progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        btnSend.setOnClickListener { v ->
+            mainViewModel.postReview(edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+        }
     }
 }
