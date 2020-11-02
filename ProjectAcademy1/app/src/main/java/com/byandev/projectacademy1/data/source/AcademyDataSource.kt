0f28324d4be2873a5db286f8a1a75958dafc0da2
byansanur <ratbyansa25@gmail.com>
2020-11-02 12:16:43 +0700

    implement live data on project academy

--- ProjectAcademy1/app/src/main/java/com/byandev/projectacademy1/data/source/AcademyDataSource.kt
+++ ProjectAcademy1/app/src/main/java/com/byandev/projectacademy1/data/source/AcademyDataSource.kt
@@ -1,17 +1,18 @@
 package com.byandev.projectacademy1.data.source
 
+import androidx.lifecycle.LiveData
 import com.byandev.projectacademy1.data.source.local.entity.CourseEntity
 import com.byandev.projectacademy1.data.source.local.entity.ModuleEntity
 
 interface AcademyDataSource {
 
-    fun getAllCourse() : List<CourseEntity>
+    fun getAllCourse() : LiveData<List<CourseEntity>>
 
-    fun getBookmarkedCourse() : List<CourseEntity>
+    fun getBookmarkedCourse() : LiveData<List<CourseEntity>>
 
-    fun getCourseWithModules(courseId: String): CourseEntity
+    fun getCourseWithModules(courseId: String): LiveData<CourseEntity>
 
-    fun getAllModulesByCourse(courseId: String): List<ModuleEntity>
+    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>>
 
-    fun getContent(courseId: String, moduleId: String): ModuleEntity
+    fun getContent(courseId: String, moduleId: String): LiveData<ModuleEntity>
 }
--- ProjectAcademy1/app/src/main/java/com/byandev/projectacademy1/ui/academy/AcademyViewModel.kt
+++ ProjectAcademy1/app/src/main/java/com/byandev/projectacademy1/ui/academy/AcademyViewModel.kt
@@ -1,5 +1,6 @@
 package com.byandev.projectacademy1.ui.academy
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.byandev.projectacademy1.data.source.AcademyRepository
 import com.byandev.projectacademy1.data.source.local.entity.CourseEntity
@@ -7,5 +8,5 @@ import com.byandev.projectacademy1.data.source.local.entity.CourseEntity
 class AcademyViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
 
 //    fun getCourse() : List<CourseEntity> = DataDummy.generateDummyCourse()
-    fun getCourse() : List<CourseEntity> = academyRepository.getAllCourse()
+    fun getCourse() : LiveData<List<CourseEntity>> = academyRepository.getAllCourse()
 }
