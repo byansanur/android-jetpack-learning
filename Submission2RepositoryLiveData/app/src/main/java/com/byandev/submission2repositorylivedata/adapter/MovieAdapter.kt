2ff340cf2d58dd9ed45e5973746f41330e335dd6
byansanur <ratbyansa25@gmail.com>
2020-11-09 23:02:28 +0700

    progress 45% of 100% - DetailActivity.kt (progress) soon : - detail view element in DetailActivity - testing

--- Submission2RepositoryLiveData/app/src/main/java/com/byandev/submission2repositorylivedata/adapter/MovieAdapter.kt
+++ Submission2RepositoryLiveData/app/src/main/java/com/byandev/submission2repositorylivedata/adapter/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.byandev.submission2repositorylivedata.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.byandev.submission2repositorylivedata.R
 import com.byandev.submission2repositorylivedata.data.repository.remote.NowPlayingResult
+import com.byandev.submission2repositorylivedata.ui.DetailActivity
 import com.byandev.submission2repositorylivedata.utils.Constant.Companion.IMAGE_URL
 import com.byandev.submission2repositorylivedata.utils.ConvertDate
 import kotlinx.android.synthetic.main.imgae_rounded.view.*
@@ -32,6 +34,11 @@ class MovieAdapter(private val context: Context?) : RecyclerView.Adapter<MovieAd
                     .into(imgThumb)
                 tvDate.text = ConvertDate.convertReleaseDate(movieList.release_date)
                 tvRating.text = movieList.vote_average.toString()
+                setOnClickListener {
+                    val intent = Intent(context, DetailActivity::class.java)
+                    intent.putExtra("idMovie", movieList.id.toString())
+                    context?.startActivity(intent)
+                }
             }
         }
     }
